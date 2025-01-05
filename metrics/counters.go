970871b8026b5f66e350d59c3a10b7311cddd300
Lucian I. Last <li@last.nl>
2025-01-05 16:17:31 +0100

    Add better descriptions to prometheus

--- metrics/counters.go
+++ metrics/counters.go
@@ -8,58 +8,64 @@ import (
 var (
 	TotalBufferContentsFull = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_buffer_contents_full_total",
-		Help: "Tigerbeetle buffer filled size is full",
+		Help: "Amount of payloads / max size of the buffer = 100 percent",
 	})
 	TotalBufferContentsLt80 = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_buffer_contents_lt80_total",
-		Help: "Tigerbeetle buffer filled size is less than 80%",
+		Help: "Amount of payloads / max size of the buffer = 80 percent or less",
 	})
 	TotalBufferContentsGte80 = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_buffer_contents_gte80_total",
-		Help: "Tigerbeetle buffer filled size is greater than or equal to 80%",
+		Help: "Amount of payloads / max size of the buffer = 80 percent or more, but not 100 percent",
 	})
 
 	TotalBufferCount = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_buffer_count_total",
-		Help: "Tigerbeetle requests total buffer instances created",
+		Help: "Counter for each time the buffer is flushed",
 	})
 
 	TotalCreateTransferTx = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_create_transfers_tx_total",
-		Help: "Created transfer transactions",
+		Help: "Counter for each tranfer created",
 	})
 
 	TotalCreateTransferTxErr = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_create_transfers_tx_error_total",
-		Help: "Created transfer error transactions",
+		Help: "Counter for each error sent back for each transfer",
 	})
 
 	TotalCreateAccountsTxErr = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_create_accounts_tx_error_total",
-		Help: "Created account error transactions",
+		Help: "Counter for each account create error",
 	})
 
 	TotalTbCreateAccountsCall = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_tb_create_accounts_total",
+		Help: "Called when tigerbeetle client create_accounts is run",
 	})
 
 	TotalTbCreateTransfersCall = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_tb_create_transfers_total",
+		Help: "Called when tigerbeetle client create_transfers is run",
 	})
 
 	TotalTbLookupAccountsCall = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_tb_lookup_accounts_total",
+		Help: "Called when tigerbeetle client lookup_accounts is run",
 	})
 
 	TotalTbLookupTransfersCall = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_tb_lookup_transfers_total",
+		Help: "Called when tigerbeetle client lookup_transfers is run",
 	})
 
 	TotalTbGetAccountTransfersCall = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_tb_get_account_transfers_total",
+		Help: "Called when tigerbeetle client get_account_transfers is run",
 	})
 
 	TotalTbGetAccountBalancesCall = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "tigerbeetleapi_tb_get_account_balances_total",
+		Help: "Called when tigerbeetle client get_account_balances is run",
 	})
 )
